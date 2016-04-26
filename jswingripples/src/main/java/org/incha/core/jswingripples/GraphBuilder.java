77c635f337227ee8a26193adcd992af340b71702
Manuel Olguin <molguin@dcc.uchile.cl>
2016-04-26 15:30:57 -0300

    Implemented prepareGraph() -> build graph from EIG nodes and edges.

--- jswingripples/src/main/java/org/incha/core/jswingripples/GraphBuilder.java
+++ jswingripples/src/main/java/org/incha/core/jswingripples/GraphBuilder.java
@@ -1,8 +1,13 @@
 package org.incha.core.jswingripples;
 
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
 import org.incha.core.jswingripples.eig.JSwingRipplesEIG;
 import org.incha.core.jswingripples.eig.JSwingRipplesEIGEdge;
 import org.incha.core.jswingripples.eig.JSwingRipplesEIGNode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Manuel Olguín (molguin@dcc.uchile.cl) on 4/26/2016.
@@ -14,9 +19,11 @@ public class GraphBuilder {
     private static GraphBuilder instance = null; // singleton
 
     private JSwingRipplesEIG eig;
-    private String[] nodes;
-    private String[] edges; //TODO: Fix (tuple or struct?)
-    private GraphBuilder(){}
+    private Graph graph;
+    private GraphBuilder()
+    {
+        graph = new SingleGraph("Dependencies");
+    }
 
     public static GraphBuilder getInstance() {
         if ( instance == null )
@@ -25,6 +32,27 @@ public class GraphBuilder {
         }
 
         return instance;
+    }
+
+    public void addEIG(JSwingRipplesEIG eig)
+    {
+        this.eig = eig;
+    }
+
+    public void prepareGraph()
+    {
+        JSwingRipplesEIGNode[] eigNodes = eig.getAllNodes();
+        JSwingRipplesEIGEdge[] eigEdges = eig.getAllEdges();
+
+        for ( JSwingRipplesEIGNode node : eigNodes )
+            graph.addNode(node.getShortName());
+
+        for ( JSwingRipplesEIGEdge edge : eigEdges )
+        {
+            String eid = edge.getFromNode().getShortName() + " -> " + edge.getToNode().getShortName();
+            graph.addEdge(eid, edge.getFromNode().getShortName(), edge.getToNode().getShortName());
+        }
+
     }
 
 
