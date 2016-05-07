c031b11c518203154ea1aa2ede77ab3bfa28fd88
fcoclavero <fcoclavero32@gmail.com>
2016-05-07 12:32:31 -0300

    Optimized searcher imports.

--- jswingripples/src/main/java/org/incha/core/search/Searcher.java
+++ jswingripples/src/main/java/org/incha/core/search/Searcher.java
@@ -5,7 +5,10 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.queryParser.ParseException;
 import org.apache.lucene.queryParser.QueryParser;
-import org.apache.lucene.search.*;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
@@ -13,7 +16,6 @@ import org.apache.lucene.util.Version;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.BitSet;
 import java.util.List;
 
 /**
