ad235d56a1c78db8ebd99759843bf920e13fd16c
fcoclavero <fcoclavero32@gmail.com>
2016-05-08 00:45:03 -0300

    Minor refactoring.

--- jswingripples/src/main/java/org/incha/ui/search/NewSearchMenu.java
+++ jswingripples/src/main/java/org/incha/ui/search/NewSearchMenu.java
@@ -5,27 +5,25 @@ import javax.swing.border.BevelBorder;
 import java.awt.*;
 
 public class NewSearchMenu {
-	
-	private JTextField searchedWords;
-	private JPanel searchpanel;
-	//private Search search;   Clase que implementa la búsqueda
+
+	private JPanel searchPanel;
 	
 	/**
      * Default constructor. Se crea un panel de búsqueda para colocarlo en algún menú.
      */
 	public NewSearchMenu() {
-		searchpanel = new JPanel();
-		searchedWords = new JTextField(15);	
-        searchpanel.setBorder(new BevelBorder(BevelBorder.LOWERED));
-        searchpanel.setLayout(new FlowLayout());      
-        searchpanel.add(new JLabel("Search:"));
-        searchpanel.add(searchedWords);    
+		searchPanel = new JPanel();
+		JTextField searchedWords = new JTextField(15);
+        searchPanel.setBorder(new BevelBorder(BevelBorder.LOWERED));
+        searchPanel.setLayout(new FlowLayout());
+        searchPanel.add(new JLabel("Search:"));
+        searchPanel.add(searchedWords);
         JButton searchButton = new JButton("Go");
         searchButton.addActionListener(new StartSearchAction(searchedWords));
-        searchpanel.add(searchButton);
+        searchPanel.add(searchButton);
 	}
 	
 	public JPanel getSearchPanel() {
-		return searchpanel;
+		return searchPanel;
 	}
 }
