6b4fd50a920ae04e54ac79af1e135184dfc83bdb
Stefano Gioia <stefanogch@gmail.com>
2016-10-19 14:41:16 -0300

    MethodGranularityDependencyBuilder now directly implements JRipplesModuleInterface, instead of its sub-interface

--- jswingripples/src/main/java/org/incha/core/jswingripples/parser/MethodGranularityDependencyBuilder.java
+++ jswingripples/src/main/java/org/incha/core/jswingripples/parser/MethodGranularityDependencyBuilder.java
@@ -13,6 +13,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.eclipse.jdt.core.IMember;
 import org.incha.core.jswingripples.JRipplesDependencyGraphModuleInterface;
+import org.incha.core.jswingripples.JRipplesModuleInterface;
 import org.incha.core.jswingripples.eig.JSwingRipplesEIG;
 import org.incha.core.jswingripples.eig.JSwingRipplesEIGNode;
 import org.incha.ui.JSwingRipplesApplication;
@@ -24,41 +25,30 @@ import org.incha.ui.util.RunnableWithProgress;
  * @author Maksym Petrenko
  *
  */
-public class MethodGranularityDependencyBuilder implements  JRipplesDependencyGraphModuleInterface{
+public class MethodGranularityDependencyBuilder implements JRipplesModuleInterface {
     private static final Log log = LogFactory.getLog(MethodGranularityDependencyBuilder.class);
     private final JSwingRipplesEIG eig;
 
-    /**
-     * @param eig the eight.
-     */
-    public MethodGranularityDependencyBuilder(final JSwingRipplesEIG eig) {
-        super();
-        this.eig = eig;
-    }
+    private class AnalysisJob implements RunnableWithProgress {
 
-	private class AnalysisJob implements RunnableWithProgress {
+        public AnalysisJob(final Set<IMember> EIGNodes) {
+            if (EIGNodes==null) return;
+            if (EIGNodes.size()==0) return;
 
-		public AnalysisJob(final Set<IMember> EIGNodes) {
-			if (EIGNodes==null) return;
-			if (EIGNodes.size()==0) return;
+            for (final Iterator<IMember> iter=EIGNodes.iterator();iter.hasNext();) {
+                final JSwingRipplesEIGNode EIGnode=(JSwingRipplesEIGNode) iter.next();
+                if (EIGnode.getNodeIMember()!=null) {
+                    final JSwingRipplesEIGNode[] refferedNodes=eig.getOutgoingAnyNodeNeighbors(EIGnode);
+                    if (refferedNodes!=null)
+                        for (int i=0;i<refferedNodes.length;i++) {
+                            eig.removeEdge(eig.getEdge(EIGnode, refferedNodes[i]));
+                        }
+                }
+            }
+        }
 
-			for (final Iterator<IMember> iter=EIGNodes.iterator();iter.hasNext();) {
-				final JSwingRipplesEIGNode EIGnode=(JSwingRipplesEIGNode) iter.next();
-				if (EIGnode.getNodeIMember()!=null) {
-					final JSwingRipplesEIGNode[] refferedNodes=eig.getOutgoingAnyNodeNeighbors(EIGnode);
-					if (refferedNodes!=null)
-					for (int i=0;i<refferedNodes.length;i++) {
-						eig.removeEdge(eig.getEdge(EIGnode, refferedNodes[i]));
-					}
-				 }
-			}
-		}
-
-		/* (non-Javadoc)
-		 * @see org.incha.ui.core.RunnableWithProgress#run(org.incha.ui.core.TaskProgressMonitor)
-		 */
-		@Override
-		public void run(final TaskProgressMonitor monitor) {
+        @Override
+        public void run(final TaskProgressMonitor monitor) {
             monitor.beginTask("Building call graph",10);
             new Analyzer(eig, monitor, new InteractiveTask.TaskListener() {
                 @Override
@@ -75,46 +65,46 @@ public class MethodGranularityDependencyBuilder implements  JRipplesDependencyGr
                     monitor.done();
                 }
             }).start();
-		}
-	}
+        }
+    }
 
-	/* (non-Javadoc)
-	 * @see org.severe.jripples.defaultmodules.parsers.interfaces.JRipplesDependencyGraphModuleInterface#AnalyzeProject()
-	 */
-	@Override
-    public void AnalyzeProject() {
-		AnalysisJob job;
-		final Window window = JSwingRipplesApplication.getInstance();
-		if (window != null) {
-			try {
-				job=new AnalysisJob(null);
-				final JSwingRipplesApplication app = JSwingRipplesApplication.getInstance();
-				try {
-					ModalContext.run(job, false, app.getProgressMonitor());
-				} finally {
-					app.getProgressMonitor().done();
-				}
-			}
-			catch (final Exception e) {
-				log.error(e);
-			}
-		} else {
-			final NullMonitor monitor=new NullMonitor();
-			job=new AnalysisJob(null);
-			try {
-				job.run(monitor);
-			} catch (final Exception e) {
-				log.error(e);
-			}
-		}
-	}
+    /**
+     * @param eig the eight.
+     */
+    public MethodGranularityDependencyBuilder(final JSwingRipplesEIG eig) {
+        super();
+        this.eig = eig;
+    }
+
+    @Override
+    public void runModule() {
+        AnalysisJob job;
+        final Window window = JSwingRipplesApplication.getInstance();
+        if (window != null) {
+            try {
+                job=new AnalysisJob(null);
+                final JSwingRipplesApplication app = JSwingRipplesApplication.getInstance();
+                try {
+                    ModalContext.run(job, false, app.getProgressMonitor());
+                } finally {
+                    app.getProgressMonitor().done();
+                }
+            }
+            catch (final Exception e) {
+                log.error(e);
+            }
+        } else {
+            final NullMonitor monitor=new NullMonitor();
+            job=new AnalysisJob(null);
+            try {
+                job.run(monitor);
+            } catch (final Exception e) {
+                log.error(e);
+            }
+        }
+    }
 
     protected Analyzer createAnalyzer(final TaskProgressMonitor monitor) {
         return new Analyzer(eig, monitor);
     }
-    
-	@Override
-	public void runModule() {
-	    AnalyzeProject();
-	}
 }
