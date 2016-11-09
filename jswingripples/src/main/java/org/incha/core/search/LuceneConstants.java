251c1dcc0c9ac37cf41806eeae59390aaac02202
Stefano Gioia <stefanogch@gmail.com>
2016-11-09 17:54:39 -0300

    made all lucene constants public

--- jswingripples/src/main/java/org/incha/core/search/LuceneConstants.java
+++ jswingripples/src/main/java/org/incha/core/search/LuceneConstants.java
@@ -5,9 +5,9 @@ package org.incha.core.search;
  * Class containing constants for the Indexer and Searcher classes.
  */
 public class LuceneConstants {
-    static final String CONTENTS = "contents";
-    static final String FILE_NAME = "filename";
-    static final String FILE_PATH = "filepath";
+    public static final String CONTENTS = "contents";
+    public static final String FILE_NAME = "filename";
+    public static final String FILE_PATH = "filepath";
     public static final String INDEX_DIRECTORY_PATH = System.getProperty("user.dir") + "/.SearchIndexes";
-    static final int MAX_RESULTS_ITEMS = 10000;
+    public static final int MAX_RESULTS_ITEMS = 10000;
 }
